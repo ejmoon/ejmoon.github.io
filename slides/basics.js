b21ce7b138f65b737a1ecbd5488fcb9daaf28305
문은주 <41390496+ejmoon@users.noreply.github.com>
2023-05-14 22:01:50 +0900

    Add files via upload

--- slides/basics.js
+++ slides/basics.js
@@ -4,19 +4,19 @@ import Slide from './slide';
 import { jsx as _jsx } from "react/jsx-runtime";
 export default {
   slides: [/*#__PURE__*/_jsx(Slide, {
-    children: /*#__PURE__*/_jsx("h2", {
+    children: /*#__PURE__*/_jsx("h3", {
       children: "\uC2B9\uD604\uC774\uC640 \uC9C0\uC778\uC774\uB294 \uD574\uC678 \uC5F0\uC218\uD504\uB85C\uADF8\uB7A8\uC5D0\uC11C \uB9CC\uB0AC\uC5B4\uC694"
     })
   }), /*#__PURE__*/_jsx(Slide, {
-    children: /*#__PURE__*/_jsx("h2", {
+    children: /*#__PURE__*/_jsx("h3", {
       children: "\uC774\uB54C\uB294 \uBAB0\uB790\uC5B4\uC694"
     })
   }), /*#__PURE__*/_jsx(Slide, {
-    children: /*#__PURE__*/_jsx("h2", {
+    children: /*#__PURE__*/_jsx("h3", {
       children: "\uB458\uC774 \uC0AC\uADC0\uAC8C \uB420 \uC904..."
     })
   }), /*#__PURE__*/_jsx(Slide, {
-    children: /*#__PURE__*/_jsx("h2", {
+    children: /*#__PURE__*/_jsx("h3", {
       children: "\uC774\uB550 \uC9C4\uC9DC \uC548 \uCE5C\uD588\uAC70\uB4E0\uC694"
     })
   }), /*#__PURE__*/_jsx(Slide, {
@@ -28,7 +28,7 @@ export default {
       }
     })
   }), /*#__PURE__*/_jsx(Slide, {
-    children: /*#__PURE__*/_jsx("h2", {
+    children: /*#__PURE__*/_jsx("h3", {
       children: "\uADF8\uB9AC\uACE0 \uC778\uB3C4\uB124\uC2DC\uC544 \uD604\uC9C0\uB85C \uACF5\uBD80\uD558\uB7EC \uAC14\uB2F5\uB2C8\uB2E4"
     })
   })]
